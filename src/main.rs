05f90dc58b8daf563a72ed58643968b0eae63b55
SIQUPN <miha.07082013@gmail.com>
2026-03-10 01:02:53 +0700

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 fn main() {
     println!("===Pipu?===");
     let mut a = String::new();
-    io::stidn().read_line(&mut a).expect("Error");
+    io::stdin().read_line(&mut a).expect("Error");
     loop {
         println!("{}", a);
     }
