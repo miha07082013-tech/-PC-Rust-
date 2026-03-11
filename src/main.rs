ae34c311282f395af4854031cbb9e0998ef51dc2
SIQUPN <miha.07082013@gmail.com>
2026-03-11 15:27:56 +0000

    Add new src

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+fn main() {
+    println!("Okay?");
+    println!("===Pipu?===");
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).expect("Error");
+    loop {
+        println!("{}", a);
+        println!("Press CNTRL + C for stop program");
+    }
+}
